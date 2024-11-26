bee02b160e5fae6b55773ea38ed5344f56506cc9
Stepan <eremeevst@gmail.com>
2024-11-26 20:36:49 +0300

    replace tabs to spaces

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,52 +1,62 @@
 package main
 
 import (
-	"log"
-	"os"
+    "log"
+    "os"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/joho/godotenv"
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+    "github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load()
-	token := os.Getenv("TOKEN")
+    godotenv.Load()
+    token := os.Getenv("TOKEN")
 
-	bot, err := tgbotapi.NewBotAPI(token)
-	if err != nil {
-		log.Panic(err)
-	}
+    bot, err := tgbotapi.NewBotAPI(token)
+    if err != nil {
+        log.Panic(err)
+    }
 
-	bot.Debug = true
+    bot.Debug = true
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+    log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
 
-	updates := bot.GetUpdatesChan(u)
+    updates := bot.GetUpdatesChan(u)
 
-	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+    for update := range updates {
+        if update.Message != nil { // If we got a message
+            log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-			switch update.Message.Command() {
-			case "help":
-				commandHelp(bot, update.Message)
-			default:
-				defaultBehavior(bot, update.Message)
-			}
-		}
-	}
+            switch update.Message.Command() {
+            case "help":
+                commandHelp(bot, update.Message)
+            case "list":
+                commandList(bot, update.Message)
+            default:
+                defaultBehavior(bot, update.Message)
+            }
+        }
+    }
 }
 
 func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
-	msg.ReplyToMessageID = inputMessage.MessageID
-	bot.Send(msg)
+    msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
+    msg.ReplyToMessageID = inputMessage.MessageID
+    bot.Send(msg)
 }
 
 func commandHelp(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "/help &mdash; help")
-	bot.Send(msg)
+    msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
+        "/help — help"
+        + "\n/list — list of products"
+    )
+    bot.Send(msg)
+}
+
+func commandList(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+    msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "List")
+    bot.Send(msg)
 }
