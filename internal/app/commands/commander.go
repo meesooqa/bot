d1b35fd859226869e6f5b696f227befee3d2540d
Stepan <eremeevst@gmail.com>
2024-11-27 18:59:10 +0300

    separate commands

--- internal/app/commands/commander.go
+++ internal/app/commands/commander.go
@@ -19,30 +19,3 @@ func NewCommander(
 		productService: productService,
 	}
 }
-
-func (c *Commander) Default(inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
-	msg.ReplyToMessageID = inputMessage.MessageID
-	c.bot.Send(msg)
-}
-
-func (c *Commander) Help(inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
-		"/help — help"+
-			"\n/list — list of products",
-	)
-	c.bot.Send(msg)
-}
-
-func (c *Commander) List(inputMessage *tgbotapi.Message) {
-	outputMsgText := "Here all the products:"
-
-	products := c.productService.List()
-	for _, p := range products {
-		outputMsgText += "\n"
-		outputMsgText += p.Title
-	}
-
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMsgText)
-	c.bot.Send(msg)
-}
