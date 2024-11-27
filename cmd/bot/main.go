4fd1cba1edbc51b4e28a9e0822cf43f038005389
Stepan <eremeevst@gmail.com>
2024-11-27 19:06:15 +0300

    add HandleUpdate

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -34,15 +34,7 @@ func main() {
 	for update := range updates {
 		if update.Message != nil { // If we got a message
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			switch update.Message.Command() {
-			case "help":
-				commander.Help(update.Message)
-			case "list":
-				commander.List(update.Message)
-			default:
-				commander.Default(update.Message)
-			}
+			commander.HandleUpdate(update)
 		}
 	}
 }
