407b1e31364968579e8a5ec19c97ef50f5fdd6bd
Stepan <eremeevst@gmail.com>
2024-11-27 20:13:57 +0300

    add argument to commnd GET

--- internal/app/commands/get.go
+++ internal/app/commands/get.go
@@ -1,8 +1,24 @@
 package commands
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func (c *Commander) Get(inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "TBD")
+	args := inputMessage.CommandArguments()
+	arg, err := strconv.Atoi(args)
+	if err != nil {
+		log.Println("wrong args", args)
+		return
+	}
+
+	msg := tgbotapi.NewMessage(
+		inputMessage.Chat.ID,
+		fmt.Sprintf("successfully parsed argument: %v", arg),
+	)
 	c.bot.Send(msg)
 }
